5155b9c950e106d6d4457a8525830d7fd3468c41
Mohith Ganesh <98941802+Mohith-Ganesh@users.noreply.github.com>
2025-05-21 23:39:32 +0530

    Update Footer.tsx

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -49,7 +49,7 @@ const Footer = () => {
         
         <div className="border-t border-gray-800 mt-12 pt-8">
           <p className="text-sm text-gray-400 text-center">
-            © {new Date().getFullYear()} ZARA. All rights reserved.
+            © {new Date().getFullYear()} STREET WEAR. All rights reserved.
           </p>
         </div>
       </div>
