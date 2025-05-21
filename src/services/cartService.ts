ea3097afd98d02c88f49adde7bf31838e9df8a66
Mohith Ganesh <98941802+Mohith-Ganesh@users.noreply.github.com>
2025-05-21 09:53:11 +0530

    Update cartService.ts

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://35.154.232.105:3000';
+const API_URL = 'http://35.154.232.105:443';
 
 export const getCart = async (token: string) => {
   const response = await fetch(`${API_URL}/cart`, {
